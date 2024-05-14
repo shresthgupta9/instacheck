aae1d4623a2e9a5e41039a8a1fd36ed81be2864d
Shresth Raj Gupta <shresthgupta.phy21@kgpian.iitkgp.ac.in>
2024-05-15 01:32:04 +0530

    finished script writing and tested

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# instacheck
+This is a script aimed to provide all the followers and following of an instagram user. It also determines which user are you following but they are not.
+
+To use this script, you must have nodejs installed on your machine.
+
+Steps to use:
+
+1. open terminal/powershell in this folder and type "npm i" and press enter. This will install all dependencies necessary for script.
+
+2. make "credentials.txt" file in this folder and put your username and password in this txt file by seperating them on a new line. Ex  
+   username  
+   password
+
+3. type "node index" in terminal/powershell and wait for the script to finish. The output will be stored in "followers.txt", "following.txt" and "notfollowing.txt" files.
--- index.js
+++ index.js
@@ -1,169 +1,256 @@
-let fs = require("fs");
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 
 const website = "https://www.instagram.com";
-const uname = "excitedjerk";
-const pword = "nikalsala";
+const credentials = fs.readFileSync("credentials.txt", "utf-8").split("\n").filter(Boolean);
+const uname = credentials[0];
+const pword = credentials[1];
+
+const binSearch = (arr, ele) => {
+	let low = 0;
+	let high = arr.length - 1;
+
+	while (low <= high) {
+		let mid = Math.floor((low + high) / 2);
+
+		if (arr[mid] === ele) {
+			return true;
+			// break
+		} else if (arr[mid] < ele) low = mid + 1;
+		else if (arr[mid] > ele) high = mid - 1;
+	}
+	return false;
+};
+
+const writeArr = (arr, txtfile) => {
+	// using filestream in case of big arrays
+	let followersFile = fs.createWriteStream(txtfile, { flags: "w" });
+	followersFile.on("error", function (err) {
+		console.log(error);
+	});
+	arr.forEach(function (item) {
+		followersFile.write(item + "\n");
+	});
+	followersFile.end();
+};
 
 async function start() {
-  try {
-    console.log("Started running script");
+	try {
+		console.log("Started running script");
 
-    // launching browser and going to instagram
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(website, { waitUntil: "networkidle0" });
+		/**************/
 
-    // filling user details and signing in
-    const unamebox = await page.$('input[name="username"]');
-    await unamebox.type(`${uname}`);
+		console.log("launching browser and going to instagram");
 
-    const pwordbox = await page.$('input[name="password"]');
-    await pwordbox.type(`${pword}`);
+		// const browser = await puppeteer.launch();
+		const browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+		await page.goto(website, { waitUntil: "networkidle0" });
 
-    const loginbtn = await page.$('button[type="submit"]');
-    await loginbtn.evaluate((loginbtn) => loginbtn.click());
+		/**************/
 
-    // dummy fn to make the script wait for some time
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+		console.log("filling user details and signing in");
 
-    // get followers and following count
-    await page.goto(`${website}/${uname}`, { waitUntil: "networkidle0" });
+		const unamebox = await page.$('input[name="username"]');
+		await unamebox.type(`${uname}`);
 
-    const followersElement = await page.waitForSelector(`a[href="/${uname}/followers/"]`);
-    const followersText = await followersElement.evaluate((ele) => ele.textContent);
-    const followersCount = parseInt(followersText.replace(/[^0-9]/g, ""), 10);
+		const pwordbox = await page.$('input[name="password"]');
+		await pwordbox.type(`${pword}`);
 
-    const followingElement = await page.waitForSelector(`a[href="/${uname}/following/"]`);
-    const followingText = await followingElement.evaluate((ele) => ele.textContent);
-    const followingCount = parseInt(followingText.replace(/[^0-9]/g, ""), 10);
+		const loginbtn = await page.$('button[type="submit"]');
+		await loginbtn.evaluate((loginbtn) => loginbtn.click());
 
-    console.log(`Followers: ${followersCount}, Following: ${followingCount}`);
+		/**************/
 
-    // // going to following page and scrolling till can't no more
-    // await page.waitForSelector(`a[href="/${uname}/following/"]`);
-    // await page.click(`a[href="/${uname}/following/"]`);
+		// dummy fn to make the script wait for some time
+		await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    // await page.waitForSelector("._aade");
+		/**************/
 
-    // let prevFollowingCnt = 0;
-    // let currFollowingCnt = await page.evaluate(() => {
-    //   return document.querySelectorAll("._aano > div > div")[0].children.length;
-    // });
+		console.log("getting followers and following count");
 
-    // console.log("Following details before-" + currFollowingCnt, prevFollowingCnt);
+		await page.goto(`${website}/${uname}`, { waitUntil: "networkidle0" });
 
-    // while (prevFollowingCnt < currFollowingCnt) {
-    //   prevFollowingCnt = currFollowingCnt;
+		const followersElement = await page.waitForSelector(`a[href="/${uname}/followers/"]`);
+		const followersText = await followersElement.evaluate((ele) => ele.textContent);
+		const followersCount = parseInt(followersText.replace(/[^0-9]/g, ""), 10);
 
-    //   await page.waitForSelector("._aacw");
+		const followingElement = await page.waitForSelector(`a[href="/${uname}/following/"]`);
+		const followingText = await followingElement.evaluate((ele) => ele.textContent);
+		const followingCount = parseInt(followingText.replace(/[^0-9]/g, ""), 10);
 
-    //   await page.evaluate(() => document.querySelectorAll("._aano > div > div")[0].lastChild.scrollIntoView());
+		console.log(`Followers: ${followersCount}, Following: ${followingCount}`);
 
-    //   await new Promise((resolve) => setTimeout(resolve, 2000));
+		/**************/
 
-    //   currFollowingCnt = await page.evaluate(() => {
-    //     return document.querySelectorAll("._aano > div > div")[0].children.length;
-    //   });
-    // }
+		console.log("getting all following list");
 
-    // console.log("Following details after-" + currFollowingCnt, prevFollowingCnt);
+		// going to following page and scrolling till can't no more
+		await page.waitForSelector(`a[href="/${uname}/following/"]`);
+		await page.click(`a[href="/${uname}/following/"]`);
 
-    // // storing following in an array as well as in a file
-    // const followingList = await page.evaluate(() => {
-    //   const elements = document.querySelectorAll("._aade, ._aad7, ._aacx");
-    //   let arr = [];
-    //   elements.forEach((element) => {
-    //     if (element.textContent !== "Following") arr.push(element.textContent);
-    //   });
-    //   arr.sort();
-    //   return arr;
-    // });
+		for (let index = 0; index < 10000000000; index++);
 
-    // // using filestream in case of big arrays
-    // let followingFile = fs.createWriteStream("following.txt", { flags: "w" });
-    // followingFile.on("error", function (err) {
-    //   console.log(error);
-    // });
-    // followingList.forEach(function (v) {
-    //   followingFile.write(v + "\n");
-    // });
-    // followingFile.end();
+		await page.waitForSelector("._aade");
 
-    // going to follower page and scrolling till can't no more
-    await page.waitForSelector(`a[href="/${uname}/followers/"]`);
-    await page.click(`a[href="/${uname}/followers/"]`);
+		let prevFollowingCnt = 0;
+		let currFollowingCnt = await page.evaluate(() => {
+			return document.querySelectorAll("._aano > div > div")[0]?.children.length === undefined
+				? 0
+				: document.querySelectorAll("._aano > div > div")[0]?.children.length;
+		});
 
-    await page.waitForSelector("._aade");
+		while (prevFollowingCnt < currFollowingCnt) {
+			prevFollowingCnt = currFollowingCnt;
 
-    let prevFollowersCnt = 0;
-    let currFollowersCnt = await page.evaluate(() => {
-      return document.querySelectorAll("._aano > div > div")[0]?.children.length === undefined
-        ? 0
-        : document.querySelectorAll("._aano > div > div")[0]?.children.length;
-    });
+			// await page.waitForSelector("._aacw");
+			await page.waitForSelector("span[class=' _ap3b _aaco _aacw _aacx _aad7 _aade']");
 
-    console.log("Followers details-" + currFollowersCnt, prevFollowersCnt);
+			await page.evaluate(() => document.querySelectorAll("._aano > div > div")[0].lastChild.scrollIntoView());
 
-    while (prevFollowersCnt < currFollowersCnt) {
-      prevFollowersCnt = currFollowersCnt;
+			await new Promise((resolve) => setTimeout(resolve, 3000));
 
-      await page.waitForSelector("._aacw");
+			currFollowingCnt = await page.evaluate(() => {
+				return document.querySelector("._aano > div > div").children.length;
+			});
 
-      await page.evaluate(() => document.querySelectorAll("._aano > div > div")[0].lastChild.scrollIntoView());
+			console.log(prevFollowingCnt, currFollowingCnt);
+		}
 
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+		/**************/
 
-      currFollowersCnt = await page.evaluate(() => {
-        return document.querySelectorAll("._aano > div > div")[0].children.length;
-      });
-    }
-    console.log("Followers details-" + currFollowersCnt, prevFollowersCnt);
+		console.log("storing following in following.txt file");
 
-    // storing followers in an array as well as in a file
-    const followersList = await page.evaluate(() => {
-      const elements = document.querySelectorAll("._aade, ._aad7, ._aacx");
+		// storing following in an array as well as in a file
+		const followingList = await page.evaluate(async () => {
+			const elements = document.querySelectorAll("._aade, ._aad7, ._aacx");
 
-      let flag = false;
-      let suggestedEle;
-      const checkflag = document.querySelectorAll(".x18hxmgj");
-      checkflag.forEach((ele) => {
-        if (ele.textContent === "Suggested for you") {
-          flag = true;
-          suggestedEle = ele;
-        }
-      });
+			let suggestedEleFollowing = null;
+			const temp = document.querySelectorAll(".x18hxmgj");
+			temp.forEach((ele) => {
+				if (ele.textContent === "Suggested for you") suggestedEleFollowing = ele;
+			});
 
-      let arr = [];
-      elements.forEach((element) => {
-        if (element.textContent !== "Following" && element.textContent !== "Follow") {
-          if (flag) {
-            if (element.offsetTop < suggestedEle.offsetTop) arr.push(element.textContent);
-          } else {
-            arr.push(element.textContent);
-          }
-        }
-      });
-      arr.sort();
-      return arr;
-    });
+			let arr = [];
+			elements.forEach((element) => {
+				if (element.textContent !== "Following" && element.textContent !== "Follow") {
+					if (suggestedEleFollowing !== null) {
+						const distance1 = element.getBoundingClientRect().top;
+						const distance2 = suggestedEleFollowing.getBoundingClientRect().top;
 
-    // using filestream in case of big arrays
-    let followersFile = fs.createWriteStream("followers.txt", { flags: "w" });
-    followersFile.on("error", function (err) {
-      console.log(error);
-    });
-    followersList.forEach(function (v) {
-      followersFile.write(v + "\n");
-    });
-    followersFile.end();
+						if (distance1 < distance2) arr.push(element.textContent);
+					} else {
+						arr.push(element.textContent);
+					}
+				}
+			});
+			arr.sort();
+			return arr;
+		});
 
-    console.log("Ended running script");
+		writeArr(followingList, "following.txt");
 
-    // await browser.close();
-  } catch (error) {
-    console.log(error);
-  }
+		/**************/
+
+		// closing following box
+		const closeBtn = "_abl-";
+		await page.waitForSelector(`button[class="${closeBtn}"]`);
+		await page.click(`button[class="${closeBtn}"]`);
+
+		/**************/
+
+		// going to follower page and scrolling till can't no more
+		console.log("getting all followers list");
+
+		await page.waitForSelector(`a[href="/${uname}/followers/"]`);
+		await page.click(`a[href="/${uname}/followers/"]`);
+
+		for (let index = 0; index < 10000000000; index++);
+
+		await page.waitForSelector("._aade");
+
+		let prevFollowersCnt = 0;
+		let currFollowersCnt = await page.evaluate(() => {
+			return document.querySelectorAll("._aano > div > div")[0]?.children.length === undefined
+				? 0
+				: document.querySelectorAll("._aano > div > div")[0]?.children.length;
+		});
+
+		while (prevFollowersCnt < currFollowersCnt) {
+			prevFollowersCnt = currFollowersCnt;
+
+			await page.waitForSelector("._aacw");
+
+			await page.evaluate(() => document.querySelectorAll("._aano > div > div")[0].lastChild.scrollIntoView());
+
+			await new Promise((resolve) => setTimeout(resolve, 3000));
+
+			currFollowersCnt = await page.evaluate(() => {
+				return document.querySelectorAll("._aano > div > div")[0].children.length;
+			});
+
+			console.log(prevFollowersCnt, currFollowersCnt);
+		}
+
+		/**************/
+
+		// storing followers in an array as well as in a file
+		console.log("storing followers in followers.txt file");
+
+		const followersList = await page.evaluate(() => {
+			const elements = document.querySelectorAll("._aade, ._aad7, ._aacx");
+
+			let suggestedEleFollowers = null;
+			const temp = document.querySelectorAll(".x18hxmgj");
+			temp.forEach((ele) => {
+				if (ele.textContent === "Suggested for you") suggestedEleFollowers = ele;
+			});
+
+			let arr = [];
+			elements.forEach((element) => {
+				if (element.textContent !== "Following" && element.textContent !== "Follow") {
+					if (suggestedEleFollowers !== null) {
+						const distance1 = element.getBoundingClientRect().top;
+						const distance2 = suggestedEleFollowers.getBoundingClientRect().top;
+
+						if (distance1 < distance2) arr.push(element.textContent);
+					} else {
+						arr.push(element.textContent);
+					}
+				}
+			});
+			arr.sort();
+			return arr;
+		});
+
+		writeArr(followersList, "followers.txt");
+
+		/**************/
+
+		console.log("calculating who is not following you");
+
+		let notFollowing = [];
+
+		for (let i = 0; i < followingList.length - 1; i++) {
+			let flag = binSearch(followersList, followingList[i]);
+			if (!flag) {
+				notFollowing.push(followingList[i]);
+			}
+		}
+
+		console.log("storing non followers in notfollowing.txt file");
+
+		writeArr(notFollowing, "notfollowing.txt");
+
+		/**************/
+
+		console.log("Ended running script");
+
+		// await browser.close();
+	} catch (error) {
+		console.log(error);
+		console.log("Some error happened");
+	}
 }
 
 start();
